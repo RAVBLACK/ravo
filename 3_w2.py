c37c4a65037a7c42aa141666cc53d9f4c57c4954
RAV <92453425+RAVBLACK@users.noreply.github.com>
2025-08-07 22:13:32 +0530

    3_w2.py

--- 3_w2.py
+++ 3_w2.py
@@ -1,8 +1,8 @@
-bookcost=24.95
+bookCost=24.95
 numBooks = 60.0
 def cost(numBooks):
   bulkBookCost = ((bookCost * 0.60) * numBooks)
   shippingCost = (3.0 + (0.75 * (numBooks - 1)))
   totalCost = bulkBookCost + shippingCost
-  print(&quot;The total cost is:&quot;, totalCost)
+  print("The total cost is: ", totalCost)
 cost(numBooks)
