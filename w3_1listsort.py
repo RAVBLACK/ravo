c877c6258745ab916def70350cf49c77a1c413c3
RAV <92453425+RAVBLACK@users.noreply.github.com>
2025-08-14 15:08:26 +0530

    week 3 programs

---
+++ w3_1listsort.py
@@ -0,0 +1,4 @@
+#Sorting list
+l=[4,55,63,3,21,4,1,0]
+l.sort()
+print(l)
---
+++ w3_4conversiontupleandlist.py
@@ -0,0 +1,9 @@
+l=[1,2,3,4,5]
+print("In list form : " ,l)
+res=tuple(l)
+print("In tuple form: ",res) #prints tuple
+
+t=(9,8,7,6)
+print("In tuple form",t)
+res2=list(t)
+print("In list form",res2)
