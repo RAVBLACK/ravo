d6472c25856033b469bf06257bb14404b515b2c0
RAV <92453425+RAVBLACK@users.noreply.github.com>
2025-08-14 15:10:36 +0530

    w3_4conversiontupleandlist.py

--- w3_4conversiontupleandlist.py
+++ w3_4conversiontupleandlist.py
@@ -1,3 +1,4 @@
+#4)Convert Tuple to List and List to Tuple.
 l=[1,2,3,4,5]
 print("In list form : " ,l)
 res=tuple(l)
@@ -7,3 +8,9 @@ t=(9,8,7,6)
 print("In tuple form",t)
 res2=list(t)
 print("In list form",res2)
+
+#Output:
+#In list form :  [1, 2, 3, 4, 5]
+#In tuple form:  (1, 2, 3, 4, 5)
+#In tuple form:  (9, 8, 7, 6)
+#In list form:  [9, 8, 7, 6]
